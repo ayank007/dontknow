397063410e5286757f52ed6e5984d0cbe0f158b8
Ayan <57215535+ayank007@users.noreply.github.com>
2020-03-27 13:14:13 +0530

    Add files via upload

--- animate.js
+++ animate.js
@@ -10,22 +10,26 @@ $(function() {
 	})
 
 	function videoSearch(key, search, n){
-		$.get("https://www.googleapis.com/youtube/v3/search?key"+key+"&type=video&part=snippet&maxResults="+n+"&q="+search, function(data){
-			data.items.forEach(item => {
-				video = `
-					<iframe
-						width="420"
-						height="315"
-						src="http://www.youtube.com/embed/${item.id.videoId}"
-						frameborder="0"
-						allowfullscreen>
-					</iframe>
-				`
-				$("#videos").append(video)
-			});
-		})
-		
-		
+		$.get("https://www.googleapis.com/youtube/v3/search?key="
+			+key+
+			"&type=video&part=snippet&maxResults="
+			+n+
+			"&q="
+			+search, function(data){
+				data.items.forEach(item => {
+					video = `
+						<iframe
+							width="420"
+							height="315"
+							src="http://www.youtube.com/embed/${item.id.videoId}"
+							frameborder="0"
+							allowfullscreen>
+						</iframe>
+					`
+					$("#videos").append(video)
+				});
+			}
+		)
 	}
 })
 
