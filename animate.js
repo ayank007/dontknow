4913551cd13244685d5ab98c14102ef1768f33c0
Ayan <57215535+ayank007@users.noreply.github.com>
2020-03-27 12:34:27 +0530

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,66 @@
+$(function() {
+	var video=''
+	var API_KEY="AIzaSyAa5FpvRztobLfeH8Snx7BOZCsyonOi_Xk"
+	$("form").submit(function(event){
+		event.preventDefault()
+		var search=$("#search").val()
+		videoSearch(API_KEY, search, 15)
+	})
+
+	function videoSearch(key, search, n){
+		$("#videos").empty()
+		$.get("https://www.googleapis.com/youtube/v3/search?key"+key+"&type=video&part=snippet&maxResults="+n+"&q="+search, function(data){
+			data.items.forEach(item => {
+				video = `
+					<iframe
+						width="420"
+						height="315"
+						src="http://www.youtube.com/embed/${item.id.videoId}"
+						frameborder="0"
+						allowFullScreen>
+					</iframe>
+				`
+				$("#videos").append(video)
+			})
+		})
+		
+		
+	}
+})
+
+$(function(){
+	var voiceList=$("#voiceList")
+	var txtip=$("#txtip")
+	var speak=$("#speak")
+	var tts=window.speechSynthesis
+	var voices=[]
+	function GetVoices(){
+		voices=tts.getVoices();
+		voiceList.innerHTML=''
+		voices.forEach((voice) => {
+			var listItem = document.createElement('option')
+			listItem.textContent=voice.name
+			listItem.setAttribute('data-lang', voice.lang)
+			listItem.setAttribute('data-lang',voice.name)
+			voiceList.append(listItem)
+		})
+	}
+	GetVoices()
+	if(speechSynthesis!==undefined){
+		speechSynthesis.onVoiceChanged=GetVoices
+		speak.addEventListner('click',()=>{
+			var toSpeak=new speechSynthesisUtterance(txt.value)
+			var selectedVoiceName=voiceList.selectedOption[0].getAttribute('data-name')
+			voices.forEach((voice)=>{
+				if(voice.name===selectedVoiceName){
+					toSpeak.voice=voice
+				}
+			})
+			tts.speak(toSpeak)
+		})
+	}
+})
+
+$(function(){
+	$('.side-bar').theiaStickySidebar()
+})
